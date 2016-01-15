e5dfaa8d7c5326b759d0cbb038d7f959ec8778ff
hugocaillard <c.hugo.188@gmail.com>
2016-01-15 01:30:41 +0100

    Add Gist details View and gist WebView

--- src/lib/gistAPI.js
+++ src/lib/gistAPI.js
@@ -1,18 +1,21 @@
+import { token } from './configGitHub.js';
+const t = token ? `access_token=${token}` : '';
+
 const c = {
   url: 'https://api.github.com/',
 };
 
 export function getUsers(str) {
   if (str.length >= 2) {
-    return fetch(`${c.url}search/users?q=${str}`);
+    return fetch(`${c.url}search/users?${t}&q=${str}`);
   }
   return false;
 }
 
 export function getUser(user) {
-  return fetch(`${c.url}users/${user}`);
+  return fetch(`${c.url}users/${user}?${t}`);
 }
 
 export function getGists(user) {
-  return fetch(`${c.url}users/${user}/gists`);
+  return fetch(`${c.url}users/${user}/gists?${t}`);
 }
